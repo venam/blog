c7955842aa777c943a52af9b68ef34f1b5019387
venam <patrick@iotek.org>
2015-11-16 17:21:34 +0200

    updated all the design

--- assets/git.js
+++ assets/git.js
@@ -1,29 +1,38 @@
 jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
+	jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
 }
- 
+
 jQuery.fn.loadRepositories = function(username) {
-    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
-     
-    var target = this;
-    $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-        sortByName(repos);    
-     
-        var list = $('<dl/>');
-        target.empty().append(list);
-        $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com')) {
-                list.append('<paper-shadow style="margin-bottom:3ex !important;"><dt style="padding:1ex;"><a class="post-link" href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em class="post-meta">'+(this.language?('('+this.language+')'):'')+'</em></dt><dd style="padding:1ex;">' + this.description +'</dd>'+'</paper-shadow>');
-            }
-        });      
-      });
-      
-    function sortByName(repos) {
-        repos.sort(function(a,b) {
-        return a.name - b.name;
-       });
-    }
+	this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+
+	var target = this;
+	$.githubUser(username, function(data) {
+		target.html(""); //empty the content
+		var html = "";
+		var repos = data.data; // JSON Parsing
+		console.log(repos);
+		sortByName(repos);    
+
+		$(repos).each(function() {
+			if (this.name != (username.toLowerCase()+'.github.com')) {
+				html += '<div class="email">';
+				html += '<a href="'+ (this.homepage?this.homepage:this.html_url);
+				html += '"> ' + this.name + ' </a>';
+				html += ' <em class="post-meta">';
+				html += (this.language?('('+this.language+')'):'');
+				html += ' </em> ';
+				html += this.description;
+				html +='</div>';
+			}
+			target.html(html);
+		});
+	});
+
+	function sortByName(repos) {
+		repos.sort(function(a,b) {
+			return a.name - b.name;
+		});
+	}
 };
 
 
